af4169f6d5127f6ab1af1b1aa24114941561140f
GustavoSistema <gf5535125@gmail.com>
2026-08-05 10:48:36 -0500

    Components Historial de Sesiones mas Detalles de caja

--- resources/views/livewire/custom-nav-menu.blade.php
+++ resources/views/livewire/custom-nav-menu.blade.php
@@ -158,6 +158,58 @@ Change class "fixed" to "sticky" in "navbar" (l. 33) so the navbar doesn't hide 
                         @endcan
                         --}}
 
+                        {{--             OPCIONES PARA CAJA (CORREGIR OPCIONES.EGRESOS)               --}}
+                        @can('opciones.egresos')
+                            <li class="text-gray-50 py-3 pl-3 pr-4 hover:bg-gray-600 focus:bg-gray-600 rounded"
+                                x-data="{ Open: false }">
+                                <div class="inline-flex  items-center justify-between w-full transition-colors duration-150 text-gray-500  cursor-pointer"
+                                    x-on:click="Open = !Open">
+                                    <span class="inline-flex items-center space-x-6  text-sm text-white ">
+                                        <i class="fa-solid fa-cash-register text-lg"></i>
+                                        <span class="select-none">Caja</span>
+                                    </span>
+                                    <i class="fa-solid fa-caret-down ml-1  text-white w-4 h-4" x-show="!Open"></i>
+                                    <i class="fa-solid fa-caret-up ml-1  text-white w-4 h-4" x-show="Open"></i>
+                                </div>
+                                <div x-show.transition="Open" style="display:none;">
+                                    <ul x-transition:enter="transition-all ease-in-out duration-300"
+                                        x-transition:enter-start="opacity-25 max-h-0"
+                                        x-transition:enter-end="opacity-100 max-h-xl"
+                                        x-transition:leave="transition-all ease-in-out duration-300"
+                                        x-transition:leave-start="opacity-100 max-h-xl"
+                                        x-transition:leave-end="opacity-0 max-h-0"
+                                        class="mt-2 divide-y-2 divide-gray-600 overflow-hidden text-sm font-medium bg-gray-200 text-white shadow-inner rounded"
+                                        aria-label="submenu">
+
+                                        <li class="transition-colors duration-150">
+                                            <x-responsive-nav-link class="text-sm" href="{{ route('caja.abrir') }}"
+                                                :active="request()->routeIs('caja.abrir')">
+                                                Abrir caja
+                                            </x-responsive-nav-link>
+                                        </li>
+                                        <li class="transition-colors duration-150">
+                                            <x-responsive-nav-link class="text-sm" href="{{ route('caja.cerrar') }}"
+                                                :active="request()->routeIs('caja.cerrar')">
+                                                Cerrar caja
+                                            </x-responsive-nav-link>
+                                        </li>
+                                        <li class="transition-colors duration-150">
+                                            <x-responsive-nav-link class="text-sm" href="{{ route('caja.egreso') }}"
+                                                :active="request()->routeIs('caja.egreso')">
+                                                Egreso de caja
+                                            </x-responsive-nav-link>
+                                        </li>
+                                        <li class="transition-colors duration-150">
+                                            <x-responsive-nav-link class="text-sm" href="{{ route('caja.historial') }}"
+                                                :active="request()->routeIs('caja.historial')">
+                                                Historial de caja
+                                            </x-responsive-nav-link>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                        @endcan
+
                         {{--             OPCIONES PARA SERVICIOS                    --}}
                         @can('opciones.servicios')
                             <li class="text-gray-50 py-3 pl-3 pr-4 hover:bg-gray-600 focus:bg-gray-600 rounded"
@@ -182,11 +234,25 @@ Change class "fixed" to "sticky" in "navbar" (l. 33) so the navbar doesn't hide 
                                         aria-label="submenu">
 
                                         <li class="transition-colors duration-150">
+                                            <x-responsive-nav-link class="text-sm" href="{{ route('ordenes.simple.crear') }}"
+                                                :active="request()->routeIs('ordenes.simple.crear')">
+                                                Crear orden de servicio
+                                            </x-responsive-nav-link>
+                                        </li>
+                                        <li class="transition-colors duration-150">
+                                            <x-responsive-nav-link class="text-sm" href="{{ route('ordenes.simple.crear') }}"
+                                                :active="request()->routeIs('ordenes.simple.crear')">
+                                                Crear orden de conversión
+                                            </x-responsive-nav-link>
+                                        </li>
+                                        {{-- 
+                                        <li class="transition-colors duration-150">
                                             <x-responsive-nav-link class="text-sm" href="{{ route('ListaCitas') }}"
                                                 :active="request()->routeIs('ListaCitas')">
                                                 Lista citas
                                             </x-responsive-nav-link>
                                         </li>
+                                        --}}
                                     </ul>
                                 </div>
                             </li>
@@ -226,7 +292,7 @@ Change class "fixed" to "sticky" in "navbar" (l. 33) so the navbar doesn't hide 
                             </li>
                         @endcan   
                         
-                        {{--             OPCIONES PARA EXPEDIENTES                    --}}
+                        {{--             OPCIONES PARA EXPEDIENTES                    
                         @can('opciones.expedientes')
                             <li class="text-gray-50 py-3 pl-3 pr-4 hover:bg-gray-600 focus:bg-gray-600 rounded"
                                 x-data="{ Open: false }">
@@ -259,8 +325,9 @@ Change class "fixed" to "sticky" in "navbar" (l. 33) so the navbar doesn't hide 
                                 </div>
                             </li>
                         @endcan
+                        --}}
 
-                        {{--             OPCIONES PARA EGRESOS --}}
+                        {{--             OPCIONES PARA EGRESOS 
                         @can('opciones.egresos')
                             <li class="text-gray-50 py-3 pl-3 pr-4 hover:bg-gray-600 focus:bg-gray-600 rounded"
                                 x-data="{ Open: false }">
@@ -292,6 +359,7 @@ Change class "fixed" to "sticky" in "navbar" (l. 33) so the navbar doesn't hide 
                                 </div>
                             </li>
                         @endcan
+                        --}}
 
                         {{--             OPCIONES PARA CLIENTES                    
                             <li class="text-gray-50 py-3 pl-3 pr-4 hover:bg-gray-600 focus:bg-gray-600 rounded"
