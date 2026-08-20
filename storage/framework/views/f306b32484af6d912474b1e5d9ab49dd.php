682a0721b3bf68fd7bd36d92efa6b57d19c6d7df
GustavoSistema <gf5535125@gmail.com>
2026-08-20 10:31:31 -0500

    fix: dejar de rastrear cache de vistas en storage

--- storage/framework/views/f306b32484af6d912474b1e5d9ab49dd.php
+++
@@ -1,4 +0,0 @@
-<a href="/">
-    <img src="<?php echo e(asset('images/images2.png')); ?>" width="300" alt="">
-</a>
-<?php /**PATH C:\xampp\htdocs\Lifegas\resources\views/components/authentication-card-logo.blade.php ENDPATH**/ ?>
